5e27e75d32831980152476932b161dfd88c7ec69
dpwls02142 <dpwls02142@gmail.com>
2025-05-25 06:23:52 +0000

    Sync from team repo: e0ab076a7854a698d4eb5a259287b2b8c909b3a1

--- src/app/qna/page.tsx
+++ src/app/qna/page.tsx
@@ -1,3 +1,15 @@
-export default function QnA() {
-	return <div />;
+import QnaPreviewCard from './components/QnaPreviewCard';
+function QnaPage() {
+	return (
+		<div className="w-full h-full px-10">
+			<div className="flex flex-col gap-4 items-center">
+				<QnaPreviewCard />
+				<hr className="h-[1px] w-11/12 border-gray-300" />
+				<QnaPreviewCard />
+				<hr className="h-[1px] w-11/12 border-gray-300" />
+				<QnaPreviewCard />
+			</div>
+		</div>
+	);
 }
+export default QnaPage;
