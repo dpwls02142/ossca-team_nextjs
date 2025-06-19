34dec51a669fa10ea103d5c9b063cbed3f5e176a
dpwls02142 <dpwls02142@gmail.com>
2025-06-19 05:16:32 +0000

    Sync from team repo: ef9c215ff9b351bbc21accfdcfb38f6591a2266e

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,13 +1,5 @@
 import SearchPageTemplate from './SearchPageTemplate';
 
 export default function Search() {
-<<<<<<< feat/WEB-56
 	return <SearchPageTemplate />;
-=======
-	return (
-		<>
-			<SearchPageTemplate />
-		</>
-	);
->>>>>>> stage
 }
